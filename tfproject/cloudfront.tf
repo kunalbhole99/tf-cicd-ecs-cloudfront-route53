05729f8a46184e0e0c08eb4b45358875579eeb6f
kunalbhole99 <kunalbhole2699@gmail.com>
2023-06-22 14:56:50 +0530

    change

--- tfproject/cloudfront.tf
+++ tfproject/cloudfront.tf
@@ -14,18 +14,18 @@ resource "aws_cloudfront_distribution" "cdn_static_site" {
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "https-only"
+      origin_protocol_policy = "match-viewer"
       origin_ssl_protocols   = ["TLSv1.1", "TLSv1.2"]
     }
   }
 
   default_cache_behavior {
     min_ttl                = 0
-    default_ttl            = 0
-    max_ttl                = 0
-    viewer_protocol_policy = "redirect-to-https"
+    default_ttl            = 3600
+    max_ttl                = 86400
+    viewer_protocol_policy = "allow-all"
 
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "my-alb-origin"
 
@@ -64,3 +64,36 @@ resource "aws_cloudfront_distribution" "cdn_static_site" {
 #   signing_behavior                  = "always"
 #   signing_protocol                  = "sigv4"
 # }
+
+
+/*
+resource "aws_cloudfront_cache_policy" "example" {
+  name        = "example-policy"
+  comment     = "test comment"
+  default_ttl = 50
+  max_ttl     = 100
+  min_ttl     = 1
+  parameters_in_cache_key_and_forwarded_to_origin {
+    cookies_config {
+      cookie_behavior = "whitelist"
+      cookies {
+        items = ["example"]
+      }
+    }
+    headers_config {
+      header_behavior = "whitelist"
+      headers {
+        items = ["example"]
+      }
+    }
+    query_strings_config {
+      query_string_behavior = "whitelist"
+      query_strings {
+        items = ["example"]
+      }
+    }
+  }
+}
+
+
+*/
